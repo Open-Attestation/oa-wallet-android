0b8ec7561ba6d5be5ebd262bebb0241d70330285
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-12 10:38:20 +0800

    chore: rename MainActivity to WalletActivity

--- app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
+++ app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
@@ -3,7 +3,7 @@ package com.example.oa_wallet_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WalletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
