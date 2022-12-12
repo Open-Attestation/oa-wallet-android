a270a08e056fc863c35e8bd30ceccbe110adbfca
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-12 13:55:15 +0800

    feat: add button Action bar

--- app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
+++ app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
@@ -2,6 +2,7 @@ package com.example.oa_wallet_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class WalletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,10 @@ class WalletActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.title = "Wallet"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.walletappbarmenu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
