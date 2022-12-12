87ec637e206bfce6bc29de707b6c0e3033bf1b32
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-12 16:07:33 +0800

    feat: open the file picker when tapping on the import button

--- app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
+++ app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
@@ -1,11 +1,19 @@
 package com.example.oa_wallet_android
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
 
 class WalletActivity : AppCompatActivity() {
+    val openFileActivityLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
+        // Handle the returned Uri
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,6 +28,7 @@ class WalletActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_import -> {
+            openFileActivityLauncher.launch(arrayOf("*/*"))
             true
         }
         else -> {
