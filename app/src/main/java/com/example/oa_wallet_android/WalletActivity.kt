3ad8c4469a67a9f2001002961e91addd01222f51
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-12 14:18:18 +0800

    feat: add handler for tapping on import button

--- app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
+++ app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
@@ -3,6 +3,7 @@ package com.example.oa_wallet_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class WalletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +14,16 @@ class WalletActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.walletappbarmenu,menu)
+        menuInflater.inflate(R.menu.walletappbarmenu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_import -> {
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
