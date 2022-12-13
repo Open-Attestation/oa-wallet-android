c58d560b854ae3b6dba6a7982909b273772defbd
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-13 11:29:26 +0800

    feat: show alert if user picks an invalid file type

--- app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
+++ app/src/main/java/com/example/oa_wallet_android/WalletActivity.kt
@@ -1,18 +1,33 @@
 package com.example.oa_wallet_android
 
-import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.DocumentsContract
+import android.provider.OpenableColumns
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
 
 class WalletActivity : AppCompatActivity() {
-    val openFileActivityLauncher = registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
-        // Handle the returned Uri
-    }
+    val openFileActivityLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            if (uri != null) {
+                val filename = getFileName(uri)
+                val extension = filename?.split('.')?.last()
+                if (extension != "oa") {
+                    val alertDialogBuilder = AlertDialog.Builder(this)
+                    alertDialogBuilder.setTitle("Invalid file type chosen.")
+                    alertDialogBuilder.setMessage("Only .oa files can be imported.")
+
+                    alertDialogBuilder.setPositiveButton("Dismiss") { _, _ ->
+
+                    }
+                    alertDialogBuilder.show()
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +36,7 @@ class WalletActivity : AppCompatActivity() {
         supportActionBar?.title = "Wallet"
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.walletappbarmenu, menu)
         return super.onCreateOptionsMenu(menu)
@@ -28,11 +44,34 @@ class WalletActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_import -> {
-            openFileActivityLauncher.launch(arrayOf("*/*"))
+            openFileActivityLauncher.launch("*/*")
             true
         }
         else -> {
             super.onOptionsItemSelected(item)
         }
     }
+
+    fun getFileName(uri: Uri): String? {
+        var result: String? = null
+        if (uri.scheme == "content") {
+            val cursor = contentResolver.query(uri, null, null, null, null)
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                }
+            } finally {
+                cursor!!.close()
+            }
+        }
+        if (result == null) {
+            result = uri.path
+            val cut = result!!.lastIndexOf('/')
+            if (cut != -1) {
+                result = result.substring(cut + 1)
+            }
+        }
+        return result
+    }
 }
+
