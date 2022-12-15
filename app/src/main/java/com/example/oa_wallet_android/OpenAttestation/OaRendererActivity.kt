2a8d607e49ad585a45985e12cc7d848cd98a7884
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2022-12-15 13:26:29 +0800

    feat: implement view document

--- app/src/main/java/com/example/oa_wallet_android/OpenAttestation/OaRendererActivity.kt
+++ app/src/main/java/com/example/oa_wallet_android/OpenAttestation/OaRendererActivity.kt
@@ -2,15 +2,20 @@ package com.example.oa_wallet_android.OpenAttestation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AlertDialog
 import com.example.oa_wallet_android.R
+import org.json.JSONObject
 
 class OaRendererActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val OA_DOCUMENT_KEY = "oadocument"
         const val OA_DOCUMENT_FILENAME_KEY = "oadocumentfilename"
     }
 
+    var isDocumentRendered = false
     var filename: String? = null
     var oaDocument: String? = null
 
@@ -31,6 +36,61 @@ class OaRendererActivity : AppCompatActivity() {
                 finish()
             }
             alertDialogBuilder.show()
+        } else {
+            val webView = WebView(this)
+            webView.webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    super.onPageFinished(view, url)
+
+                    if (isDocumentRendered) {
+                        return
+                    }
+
+                    val js = "getData($oaDocument);"
+                    webView.evaluateJavascript(js) { result ->
+                        try {
+                            val jsonObj = JSONObject(result)
+                            val template = jsonObj.getJSONObject("\$template")
+                            val templateUrl = template["url"].toString()
+
+                            var rendererHtml =
+                                application.assets.open("oarenderer.html").bufferedReader().use {
+                                    it.readText()
+                                }
+
+                            rendererHtml =
+                                rendererHtml.replace("<TEMPLATE_RENDERER_URL>", templateUrl)
+                            rendererHtml = rendererHtml.replace("<OA_DOCUMENT>", jsonObj.toString())
+
+                            isDocumentRendered = true
+                            webView.loadDataWithBaseURL(
+                                null,
+                                rendererHtml,
+                                "text/html",
+                                "utf-8",
+                                null
+                            );
+
+                        } catch (e: Exception) {
+                            Log.e("", e.toString())
+                        }
+                    }
+                }
+            }
+
+            webView.setInitialScale(1);
+            webView.settings.loadWithOverviewMode = true
+            webView.settings.useWideViewPort = true;
+            webView.settings.javaScriptEnabled = true
+
+            setContentView(webView)
+            webView.loadDataWithBaseURL(
+                null,
+                "<html><head></head><body><script src=\"file:///android_asset/oabundle.js\" type=\"text/javascript\"></script>Initialising</body></html>",
+                "text/html",
+                "utf-8",
+                null
+            );
         }
     }
 }
