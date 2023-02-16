ab194461bc5bcecbfb28d958b1878a819e0b2b56
Tan Cher Shen <tan_cher_shen@tech.gov.sg>
2023-02-16 17:36:35 +0800

    feat: create ScannerFragment

--- app/src/main/java/com/example/oa_wallet_android/ui/scanner/ScannerFragment.kt
+++ app/src/main/java/com/example/oa_wallet_android/ui/scanner/ScannerFragment.kt
@@ -1,15 +1,31 @@
 package com.example.oa_wallet_android.ui.scanner
 
+import android.Manifest.permission.CAMERA
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.oa_wallet_android.R
 import com.example.oa_wallet_android.databinding.FragmentScannerBinding
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
 
 class ScannerFragment : Fragment() {
+    private lateinit var cameraSource: CameraSource
+    private lateinit var barcodeDetector: BarcodeDetector
+
+    private var scannedValue = ""
 
     private var _binding: FragmentScannerBinding? = null
 
@@ -17,26 +33,118 @@ class ScannerFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted ->
+            // Handle Permission granted/rejected
+            if (isGranted) {
+                // Permission is granted
+                setupControls()
+
+                val navController = findNavController()
+                navController.run {
+                    popBackStack()
+                    navigate(R.id.navigation_scanner)
+                }
+            } else {
+                // Permission is denied
+                Toast.makeText(requireActivity(), "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentScannerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(), CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            askForCameraPermission()
+        } else {
+            setupControls()
         }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        cameraSource.stop()
         _binding = null
+    }
+
+    private fun setupControls() {
+        barcodeDetector =
+            BarcodeDetector.Builder(requireActivity()).setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build()
+
+        cameraSource = CameraSource.Builder(requireActivity(), barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true)
+            .build()
+
+        binding.cameraSurfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    //Start preview after 1s delay
+                    cameraSource.start(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                try {
+                    cameraSource.start(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+        })
+
+
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+                Toast.makeText(context, "Scanner has been closed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+                val barcodes = detections.detectedItems
+                if (barcodes.size() == 1) {
+                    scannedValue = barcodes.valueAt(0).rawValue
+                    requireActivity().runOnUiThread {
+                        cameraSource.stop()
+                        Toast.makeText(
+                            requireActivity(),
+                            "value- $scannedValue",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        })
+    }
+
+    private fun askForCameraPermission() {
+        activityResultLauncher.launch(CAMERA)
     }
 }
